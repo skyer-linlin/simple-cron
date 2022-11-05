265d51014acff982e232337d1c91aa0e9d79970d
quanlinlin <linlink22@gmail.com>
2022-11-06 01:20:17 +0800

    fix(抓取数据): 更新主键自增策略,以前不理解用错了

--- src/main/kotlin/com/lin/simplecron/domain/CoinglassCmarginFundingRate.java
+++ src/main/kotlin/com/lin/simplecron/domain/CoinglassCmarginFundingRate.java
@@ -27,7 +27,7 @@ import java.util.Objects;
 public class CoinglassCmarginFundingRate implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     /**
      * symbol
